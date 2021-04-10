23247162671152ce5e9622014e8d0e8262426933
Josep Sayol <josep.sayol@gmail.com>
2021-04-10 18:09:02 +0200

    Multiple themes

--- src/background.ts
+++ src/background.ts
@@ -1,113 +1,192 @@
-interface Options {
-    locale?: string;
+// TODO: webpack or similar
+
+interface Settings {
+  locale: string;
+  theme: string;
 }
 
-let options: Options = {};
-
-//
-// Define and install chrome.webRequest listeners.
-//
-function onBeforeRequestListener(details: chrome.webRequest.WebRequestDetails): chrome.webRequest.BlockingResponse | void {
-    if (options.locale && (options.locale !== 'default')) {
-        const match = details.url.match(new RegExp('^(https?://trader.degiro.nl/i18n/messages_)(.+)'));
-
-        if (match !== null && Array.isArray(match)) {
-            const [, urlPrefix, requestedLocale] = match;
-            console.log(details, { requestedLocale, locale: options.locale });
-
-            if (requestedLocale !== options.locale) {
-                const redirect: chrome.webRequest.BlockingResponse = {
-                    redirectUrl: urlPrefix + options.locale
-                };
-                return redirect;
-            }
-
-        }
-    }
+const DEFAULT_SETTINGS: Settings = {
+  locale: "default",
+  theme: "default",
 };
 
-function onHeadersReceivedListener(details: chrome.webRequest.WebResponseHeadersDetails): chrome.webRequest.BlockingResponse | void {
-    if (/*details.url.includes('orderMode') &&*/ details.responseHeaders) {
-        const responseHeaders = details.responseHeaders.filter(h => h.name !== "x-frame-options");
+let settings = DEFAULT_SETTINGS;
 
-        if (details.responseHeaders.some(h => h.name === "x-frame-options")) {
-            console.log({ details, responseHeaders });
-        }
-
-        return { responseHeaders };
+function getSettings(callback: (o: Settings) => any) {
+  chrome.storage.sync.get(DEFAULT_SETTINGS, (settings) => {
+    if (callback) {
+      callback(settings as Settings);
     }
+  });
 }
 
-function onErrorOccurredListener(details: chrome.webRequest.WebResponseErrorDetails) {
-    console.log('onErrorOccurredListener', details);
-};
+function syncSetting(name: string, value: any, callback?: Function) {
+  console.log(name in settings, settings[name as keyof Settings] !== value);
+  if (name in settings && settings[name as keyof Settings] !== value) {
+    console.log("Syncing", name, value);
+    const setting = {
+      [name]: value,
+    };
 
-chrome.webRequest.onErrorOccurred.addListener(onErrorOccurredListener, { urls: ["<all_urls>"] });
-chrome.webRequest.onBeforeRequest.addListener(onBeforeRequestListener, { urls: ["<all_urls>"] }, [
+    settings[name as keyof Settings] = value;
+
+    chrome.storage.sync.set(setting, () => {
+      if (callback) {
+        callback();
+      }
+      // chrome.runtime.sendMessage('reload', () => {
+      //     window.close();
+      // });
+
+      propagateSettingsUpdate(settings);
+    });
+
+    // if (name === "theme") {
+    //   applyCustomTheme(value);
+    // }
+  }
+}
+
+// Intercept the request for the I18N file and replace it with the user's preferred language
+function onBeforeRequestListener(
+  details: chrome.webRequest.WebRequestDetails
+): chrome.webRequest.BlockingResponse | void {
+  if (settings.locale && settings.locale !== "default") {
+    const match = details.url.match(
+      new RegExp("^(https?://trader.degiro.nl/i18n/messages_)(.+)")
+    );
+
+    if (match !== null && Array.isArray(match)) {
+      const [, urlPrefix, requestedLocale] = match;
+
+      if (requestedLocale !== settings.locale) {
+        const redirect: chrome.webRequest.BlockingResponse = {
+          redirectUrl: urlPrefix + settings.locale,
+        };
+        return redirect;
+      }
+    }
+  }
+}
+
+// Remove the "x-frame-options" response header so we can show the quick order page in an iframe
+function onHeadersReceivedListener(
+  details: chrome.webRequest.WebResponseHeadersDetails
+): chrome.webRequest.BlockingResponse | void {
+  if (details.responseHeaders) {
+    const responseHeaders = details.responseHeaders.filter(
+      (h) => h.name !== "x-frame-options"
+    );
+
+    // if (details.responseHeaders.some(h => h.name === "x-frame-options")) {
+    //     console.log({ details, responseHeaders });
+    // }
+
+    return { responseHeaders };
+  }
+}
+
+function onErrorOccurredListener(
+  details: chrome.webRequest.WebResponseErrorDetails
+) {
+  console.log("onErrorOccurredListener", details);
+}
+
+chrome.webRequest.onErrorOccurred.addListener(onErrorOccurredListener, {
+  urls: ["<all_urls>"],
+});
+chrome.webRequest.onBeforeRequest.addListener(
+  onBeforeRequestListener,
+  { urls: ["<all_urls>"] },
+  [
     // options: "blocking", "requestBody"
     "blocking",
-]);
-chrome.webRequest.onHeadersReceived.addListener(onHeadersReceivedListener, { urls: ["<all_urls>"] }, [
+  ]
+);
+chrome.webRequest.onHeadersReceived.addListener(
+  onHeadersReceivedListener,
+  { urls: ["<all_urls>"] },
+  [
     // options: "blocking", "responseHeaders", or "extraHeaders"
-    "blocking", "responseHeaders",
-]);
+    "blocking",
+    "responseHeaders",
+  ]
+);
 
 let onStartupOrOnInstalledListener = function () {
-    loadOptions();
+  console.log("onStartupOrOnInstalledListener");
+  getSettings((items) => {
+    console.log(settings);
+    settings = items;
+    propagateSettingsUpdate(settings);
+  });
 
-    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-        console.log({ message, sender });
+  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    console.log({ message, sender });
 
-        if (message === 'reload') {
-            loadOptions(() => {
-                chrome.tabs.executeScript({ code: 'window.location.reload();' });
-            })
-        }
-
-        sendResponse();
-    });
+    if (message === "reload") {
+      chrome.tabs.executeScript({ code: "window.location.reload();" });
+      sendResponse();
+    } else if ("op" in message) {
+      console.log("op in message");
+      //   if (message.op === "getSettings") {
+      //     getSettings((list) => sendResponse(list));
+      //   } else
+      if (message.op === "saveSetting") {
+        console.log("saveSetting");
+        const { name, value } = message;
+        syncSetting(name, value, () => sendResponse());
+      } else if (message.op === "getSettings") {
+        sendResponse(settings);
+        // propagateSettingsUpdate(settings);
+      }
+    } else {
+      sendResponse();
+    }
+  });
 };
-
-function loadOptions(callback?: (o?: Options) => any) {
-    console.log('Loading options...');
-    chrome.storage.sync.get({
-        locale: 'default'
-    }, function (items) {
-        options = items;
-        console.log(options);
-
-        if (callback) {
-            callback(options);
-        }
-    });
-}
 
 // happens on browser start
 chrome.runtime.onStartup.addListener(function () {
-    onStartupOrOnInstalledListener();
+  onStartupOrOnInstalledListener();
 });
 
 // happens on Reload of extension
 chrome.runtime.onInstalled.addListener(function () {
-    onStartupOrOnInstalledListener();
+  onStartupOrOnInstalledListener();
 });
 
-// chrome.tabs.onCreated.addListener(applyCustomStyles);
 chrome.tabs.onUpdated.addListener((tabId, info, tab) => {
-    if (info.status == 'complete') applyCustomStyles(tab);
+  //   if (info.status == "complete") enableCustomThemesForTab(tab);
 });
 
-let customStylesApplied = false;
+// function enableCustomThemesForTab(tab: chrome.tabs.Tab) {
+//   var tabUrl = tab.url;
+//   if (tabUrl && tabUrl.match("^https?://trader.degiro.nl")) {
+//     console.log(`enableCustomThemesForTab(${tab.id})`);
+//     chrome.tabs.insertCSS(tab.id as number, { file: "css/theme.css" });
+//   }
+// }
 
-function applyCustomStyles(tab: chrome.tabs.Tab, force = false) {
-    var tabUrl = tab.url;
-    if (tabUrl && tabUrl.match("^https?://trader.degiro.nl")) {
-        if (!customStylesApplied || force) {
-            console.log('applyCustomStyles()');
-            chrome.tabs.insertCSS(tab.id as number, {
-                file: "css/dark.css"
-            });
-            customStylesApplied = true;
-        }
-    }
+// function applyCustomTheme(theme: string) {
+//   console.log(`applyCustomTheme(${theme})`);
+//   chrome.runtime.sendMessage({ op: "applyCustomTheme", theme });
+//   //   chrome.tabs.query({ url: "*://trader.degiro.nl/*" }, (tabs) => {
+//   //     console.log(tabs);
+//   //     // tabs.forEach((tab) => enableCustomThemesForTab(tab));
+//   //   });
+// }
+
+function propagateSettingsUpdate(settings: Settings) {
+  console.log("propagateSettingsUpdate", settings);
+
+  chrome.runtime.sendMessage({ op: "settingsUpdate", settings });
+
+  chrome.tabs.query({ url: "*://trader.degiro.nl/*" }, (tabs) => {
+    tabs.forEach((tab) => {
+      if (tab && tab.id) {
+        chrome.tabs.sendMessage(tab.id, { op: "settingsUpdate", settings });
+      }
+    });
+  });
 }
--- src/popup.ts
+++ src/popup.ts
@@ -1,77 +1,97 @@
-// Saves options to chrome.storage
-function saveOptions() {
-    const localeElement = document.getElementById('locale') as HTMLSelectElement;
-    const locale = localeElement.value;
-    const newOptions = {
-        locale
-    };
-
-    chrome.storage.sync.set(newOptions, () => {
-        console.log(newOptions);
-        chrome.runtime.sendMessage('reload', () => {
-            window.close();
-        });
-    });
+interface Settings {
+  locale: string;
+  theme: string;
 }
 
-// Restores options using the preferences stored in chrome.storage.
-function getFormOptions() {
-    // Use default value locale = 'default'
-    chrome.storage.sync.get({
-        locale: 'default'
-    }, function (items) {
-        const localeElement = document.getElementById('locale') as HTMLSelectElement;
-        localeElement.value = items.locale;
+// Saves options to chrome.storage
+function saveOptions() {
+  const localeElement = document.getElementById("locale") as HTMLSelectElement;
+
+  const locale = localeElement.value;
+  const newOptions = {
+    locale,
+  };
+
+  chrome.storage.sync.set(newOptions, () => {
+    console.log(newOptions);
+    chrome.runtime.sendMessage("reload", () => {
+      window.close();
     });
+  });
+}
+
+function saveSetting(name: string, value: any) {
+  chrome.runtime.sendMessage({ op: "saveSetting", name, value }, () => {
+    // window.close();
+  });
+}
+
+// Restores stored settings
+function loadSettings() {
+  console.log("Loading settings...");
+  chrome.runtime.sendMessage({ op: "getSettings" }, (settings: Settings) => {
+    console.log(settings);
+    const localeElement = document.getElementById(
+      "locale"
+    ) as HTMLSelectElement;
+    const themeElement = document.querySelector(
+      "#theme-" + settings.theme
+    ) as HTMLInputElement | null;
+
+    localeElement.value = settings.locale;
+    if (themeElement) {
+      themeElement.checked = true;
+    }
+  });
 }
 
 function quickOrderOpen() {
-    // const settings = document.getElementById('settings');
-    // const quickOrder = document.getElementById('quickOrder');
-    const quickOrderIframe = document.getElementById('quickOrder-iframe') as HTMLIFrameElement;
+  const quickOrderIframe = document.getElementById(
+    "quickOrder-iframe"
+  ) as HTMLIFrameElement;
 
-    document.body.classList.remove('showSettings');
+  document.body.classList.remove("showSettings");
 
-    // if (settings !== null) {
-    //     settings.style.display = 'none'
-    // }
-
-    // if (quickOrder !== null) {
-    //     quickOrder.style.display = 'block'
-    // }
-
-    if (quickOrderIframe !== null) {
-        quickOrderIframe.src = 'https://trader.degiro.nl/trader/?orderMode#/markets?newOrder';
-    }
+  if (quickOrderIframe !== null) {
+    quickOrderIframe.src =
+      "https://trader.degiro.nl/trader/?orderMode#/markets?newOrder";
+  }
 }
 
 function quickOrderClose() {
-    // const settings = document.getElementById('settings');
-    // const quickOrder = document.getElementById('quickOrder');
-    const quickOrderIframe = document.getElementById('quickOrder-iframe') as HTMLIFrameElement;
+  const quickOrderIframe = document.getElementById(
+    "quickOrder-iframe"
+  ) as HTMLIFrameElement;
 
-    document.body.classList.add('showSettings');
+  document.body.classList.add("showSettings");
 
-    // if (settings !== null) {
-    //     settings.style.display = 'none'
-    // }
-
-    // if (quickOrder !== null) {
-    //     quickOrder.style.display = 'block'
-    // }
-
-    if (quickOrderIframe !== null) {
-        quickOrderIframe.src = '';
-    }
+  if (quickOrderIframe !== null) {
+    quickOrderIframe.src = "";
+  }
 }
 
-document.addEventListener('DOMContentLoaded', getFormOptions);
+document.addEventListener("DOMContentLoaded", loadSettings);
 
-const saveButton = document.getElementById('save');
-saveButton && saveButton.addEventListener('click', saveOptions);
+const saveButton = document.getElementById("save");
+saveButton && saveButton.addEventListener("click", saveOptions);
 
-const quickOrderOpenElem = document.getElementById('quickOrder-open');
-quickOrderOpenElem && quickOrderOpenElem.addEventListener('click', quickOrderOpen);
+const quickOrderOpenElem = document.getElementById("quickOrder-open");
+quickOrderOpenElem &&
+  quickOrderOpenElem.addEventListener("click", quickOrderOpen);
 
-const quickOrderCloseElem = document.getElementById('quickOrder-close');
-quickOrderCloseElem && quickOrderCloseElem.addEventListener('click', quickOrderClose);
+const quickOrderCloseElem = document.getElementById("quickOrder-close");
+quickOrderCloseElem &&
+  quickOrderCloseElem.addEventListener("click", quickOrderClose);
+
+document
+  .querySelectorAll('input[type="radio"][name="theme"]:not([disabled])')
+  .forEach((radio) => {
+    radio.addEventListener("change", (event) => {
+      const theme = (event.target as HTMLInputElement).value;
+      saveSetting("theme", theme);
+    });
+  });
+
+document.querySelector("#locale")?.addEventListener("change", (event) => {
+  saveSetting("locale", (event.target as HTMLSelectElement).value);
+});
