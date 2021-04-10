9c09081043f47050c10b35479b6ae357508563a2
Josep Sayol <josep.sayol@gmail.com>
2021-04-10 23:20:03 +0200

    Improvements to the popup UI

--- src/popup.ts
+++ src/popup.ts
@@ -3,34 +3,23 @@ interface Settings {
   theme: string;
 }
 
-// Saves options to chrome.storage
-function saveOptions() {
-  const localeElement = document.getElementById("locale") as HTMLSelectElement;
-
-  const locale = localeElement.value;
-  const newOptions = {
-    locale,
-  };
-
-  chrome.storage.sync.set(newOptions, () => {
-    console.log(newOptions);
-    chrome.runtime.sendMessage("reload", () => {
-      window.close();
+function reloadOpenTabs() {
+  chrome.tabs.query({ url: "*://trader.degiro.nl/*" }, (tabs) => {
+    tabs.forEach((tab) => {
+      if (tab && tab.id) {
+        chrome.tabs.sendMessage(tab.id, { op: "reload" });
+      }
     });
   });
 }
 
 function saveSetting(name: string, value: any) {
-  chrome.runtime.sendMessage({ op: "saveSetting", name, value }, () => {
-    // window.close();
-  });
+  chrome.runtime.sendMessage({ op: "saveSetting", name, value });
 }
 
 // Restores stored settings
 function loadSettings() {
-  console.log("Loading settings...");
   chrome.runtime.sendMessage({ op: "getSettings" }, (settings: Settings) => {
-    console.log(settings);
     const localeElement = document.getElementById(
       "locale"
     ) as HTMLSelectElement;
@@ -45,48 +34,16 @@ function loadSettings() {
   });
 }
 
-function quickOrderOpen() {
-  const quickOrderIframe = document.getElementById(
-    "quickOrder-iframe"
-  ) as HTMLIFrameElement;
-
-  document.body.classList.remove("showSettings");
-
-  if (quickOrderIframe !== null) {
-    quickOrderIframe.src =
-      "https://trader.degiro.nl/trader/?orderMode#/markets?newOrder";
-  }
-}
-
-function quickOrderClose() {
-  const quickOrderIframe = document.getElementById(
-    "quickOrder-iframe"
-  ) as HTMLIFrameElement;
-
-  document.body.classList.add("showSettings");
-
-  if (quickOrderIframe !== null) {
-    quickOrderIframe.src = "";
-  }
-}
-
 document.addEventListener("DOMContentLoaded", loadSettings);
 
 const saveButton = document.getElementById("save");
-saveButton && saveButton.addEventListener("click", saveOptions);
-
-const quickOrderOpenElem = document.getElementById("quickOrder-open");
-quickOrderOpenElem &&
-  quickOrderOpenElem.addEventListener("click", quickOrderOpen);
-
-const quickOrderCloseElem = document.getElementById("quickOrder-close");
-quickOrderCloseElem &&
-  quickOrderCloseElem.addEventListener("click", quickOrderClose);
+saveButton && saveButton.addEventListener("click", reloadOpenTabs);
 
 document
   .querySelectorAll('input[type="radio"][name="theme"]:not([disabled])')
   .forEach((radio) => {
     radio.addEventListener("change", (event) => {
+      console.log(event);
       const theme = (event.target as HTMLInputElement).value;
       saveSetting("theme", theme);
     });
@@ -94,4 +51,21 @@ document
 
 document.querySelector("#locale")?.addEventListener("change", (event) => {
   saveSetting("locale", (event.target as HTMLSelectElement).value);
+});
+
+document.querySelector("#tab2")?.addEventListener("change", (event) => {
+  const selected = (event.target as HTMLInputElement).value === "on";
+
+  if (selected) {
+    const iframe = document.querySelector(
+      "iframe#quickOrder"
+    ) as HTMLIFrameElement;
+
+    if (!iframe.hasAttribute("src")) {
+      iframe.setAttribute(
+        "src",
+        "https://trader.degiro.nl/trader/?orderMode#/markets?newOrder"
+      );
+    }
+  }
 });
