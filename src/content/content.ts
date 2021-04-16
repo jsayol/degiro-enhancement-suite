d85a61ce20e00f901b5e244ddf14eec8744db500
Josep Sayol <josep.sayol@gmail.com>
2021-04-16 19:19:27 +0200

    Fix auto theme, some colors

--- src/content/content.ts
+++ src/content/content.ts
@@ -1,5 +1,7 @@
 import { Settings } from "../common";
 
+const BASE_THEME_ID = "--suite-theme-css";
+
 let connectionPort: chrome.runtime.Port;
 let currentTheme = "default";
 
@@ -53,6 +55,31 @@ function onMessageHandler(
   }
 }
 
+function colorSchemeChangeHandler(event: MediaQueryListEvent) {
+  if (currentTheme === "auto") {
+    applyAutoTheme(event.matches);
+  }
+}
+
+function loadBaseTheme() {
+  if (document.getElementById(BASE_THEME_ID)) {
+    return;
+  }
+  const link = document.createElement("link");
+  link.setAttribute("href", chrome.extension.getURL("content/styles/theme.css"));
+  link.setAttribute("id", BASE_THEME_ID);
+  link.setAttribute("type", "text/css");
+  link.setAttribute("rel", "stylesheet");
+  document.querySelector("html")?.appendChild(link);
+}
+
+function unloadBaseTheme() {
+  const cssNode = document.getElementById(BASE_THEME_ID);
+  if (cssNode && cssNode.parentNode) {
+    cssNode.parentNode.removeChild(cssNode);
+  }
+}
+
 function applyCustomTheme(theme: string) {
   if (!theme) {
     theme = "default";
@@ -62,22 +89,28 @@ function applyCustomTheme(theme: string) {
     if (currentTheme !== "auto") {
       // Determine which theme we should use
       const useDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-      document.querySelector("html").dataset.suiteTheme = useDark
-        ? "dark"
-        : "default";
+      applyAutoTheme(useDark);
     }
   } else {
     document.querySelector("html").dataset.suiteTheme = theme;
+    if (theme === "default") {
+      unloadBaseTheme();
+    } else {
+      loadBaseTheme();
+    }
   }
 
   currentTheme = theme;
 }
 
-function colorSchemeChangeHandler(event: MediaQueryListEvent) {
-  if (currentTheme === "auto") {
-    document.querySelector("html").dataset.suiteTheme = event.matches
-      ? "dark"
-      : "default";
+function applyAutoTheme(useDark = false) {
+  document.querySelector("html").dataset.suiteTheme = useDark
+    ? "dark"
+    : "default";
+  if (useDark) {
+    loadBaseTheme();
+  } else {
+    unloadBaseTheme();
   }
 }
 
