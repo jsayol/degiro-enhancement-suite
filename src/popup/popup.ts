78591b5482e53a93fa29dd5e70b093eb147a6950
Josep Sayol <josep.sayol@gmail.com>
2021-04-19 19:38:42 +0200

    FX fee calculator, some fixes

--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -53,22 +53,23 @@ document
     });
   });
 
-(document.querySelector("#locale") as HTMLSelectElement).addEventListener(
-  "change",
-  (event) => {
-    saveSetting("locale", (event.target as HTMLSelectElement).value);
-    (document.querySelector(
-      "#reloadButton"
-    ) as HTMLButtonElement).style.display = "block";
-  }
-);
+document.querySelector("#locale").addEventListener("change", (event) => {
+  saveSetting("locale", (event.target as HTMLSelectElement).value);
+  (document.querySelector("#reloadButton") as HTMLButtonElement).style.display =
+    "block";
+});
 
-(document.querySelector("#tab2") as HTMLInputElement).addEventListener(
-  "change",
+(document.querySelector("#tab-order") as HTMLInputElement).addEventListener(
+  "selectionchange",
   (event) => {
     const selected = (event.target as HTMLInputElement).value === "on";
+    console.log({ selected });
 
     if (selected) {
+      // Hide the donations buttons
+      (document.querySelector(".bottom") as HTMLDivElement).style.display =
+        "none";
+
       const iframe = document.querySelector(
         "iframe#quickOrder"
       ) as HTMLIFrameElement;
@@ -89,14 +90,83 @@ document
           false
         );
       }
+    } else {
+      // Show the donations buttons
+      (document.querySelector(".bottom") as HTMLDivElement).style.display =
+        "block";
     }
   }
 );
 
-(document.querySelector("#reloadButton") as HTMLButtonElement).addEventListener(
-  "click",
-  (event) => {
-    reloadOpenTabs();
-    (event.target as HTMLButtonElement).style.display = "none";
+document.querySelector("#reloadButton").addEventListener("click", (event) => {
+  reloadOpenTabs();
+  (event.target as HTMLButtonElement).style.display = "none";
+});
+
+const fxAmountElement = document.querySelector("#fx-amount");
+["keyup", "keydown", "change"].forEach((action) => {
+  fxAmountElement.addEventListener(action, fxCalculateFee);
+});
+
+document
+  .querySelector("#fx-currency")
+  .addEventListener("change", fxCalculateFee);
+
+const currencyMap = {
+  EUR: "€",
+  USD: "$",
+  GBP: "£",
+  CHF: "Fr",
+  DKK: "Kr",
+  NOK: "Kr",
+  SEK: "Kr",
+};
+
+function fxCalculateFee() {
+  const amountElement: HTMLInputElement = document.querySelector("#fx-amount");
+  const currencyElement: HTMLSelectElement = document.querySelector(
+    "#fx-currency"
+  );
+
+  const amount = Number(amountElement.value);
+  const currency = currencyElement.value;
+
+  const feeAutofx = Math.round(100 * (amount * 0.001)) / 100;
+  const feeManual = Math.round(100 * (amount * 0.0002)) / 100;
+  const feeAutofxElement = document.querySelector("#fx-fee-autofx");
+  const feeManualElement = document.querySelector("#fx-fee-manual");
+  const currBefore = ["USD", "GBP"].includes(currency);
+  const currSymbol = currencyMap[currency];
+
+  let feeAutofxDisplay = "";
+  let feeManualDisplay = "";
+
+  // TODO: this is a mess, clean it up
+  if (amount > 0) {
+    feeAutofxDisplay = `${currBefore ? currSymbol : ""}<b>${String(
+      feeAutofx
+    )}</b>${!currBefore ? currSymbol : ""}`;
+    if (currency === "EUR") {
+      feeManualDisplay = `${currBefore ? currSymbol : ""}<b>${String(
+        Math.round(100 * (10 + feeManual)) / 100
+      )}</b>${!currBefore ? currSymbol : ""}`;
+    } else {
+      feeManualDisplay = `<b>10</b>€ + ${
+        currBefore ? currSymbol : ""
+      }<b>${String(feeManual)}</b>${!currBefore ? currSymbol : ""}`;
+    }
   }
-);
+
+  feeAutofxElement.innerHTML = feeAutofxDisplay;
+  feeManualElement.innerHTML = feeManualDisplay;
+}
+
+document.querySelectorAll(".foldable-trigger").forEach((trigger) => {
+  trigger.addEventListener("click", (event) => {
+    const content = trigger.parentElement.querySelector(".foldable-content");
+    if (content) {
+      content.classList.toggle("open");
+    }
+    trigger.classList.toggle("open");
+  });
+});
